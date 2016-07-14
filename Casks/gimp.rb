316ce8399c3a7d321685ea94e32a093a2f95dde0
Sam Morrow <info@sammorrowdrums.com>
2016-07-14 11:53:39 +0100

    Gimp current download version has changed

--- Casks/gimp.rb
+++ Casks/gimp.rb
@@ -1,5 +1,5 @@
 cask 'gimp' do
-  version '2.8.16-x86_64'
+  version '2.8.16-x86_64-1'
   sha256 'bd84e6e6af21ee76c664654dc47344655086d0587de807418517dd1ec57b1e09'
 
   url "https://download.gimp.org/pub/gimp/v2.8/osx/gimp-#{version}.dmg"
